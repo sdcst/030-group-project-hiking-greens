5989fd80564b266a0d8d165e35247ab83ff9f508
fxrifty <logan.stevens@gmx.com>
2024-05-13 13:22:21 -0700

    save

--- assignment.py
+++ assignment.py
@@ -4,16 +4,18 @@
 
 import math
 def menu():
-    input("For The Volume Of A Square Enter")
+    print("For The Volume Of A Square Enter: '1'\n For The Area Of A Square Enter: '2'\n For The Volume Of A Rectangle Enter: '3'\n For The Area Of A Rectangle Enter: '4'\n For The Volume Of A Sphere Enter: '5'\n For The Area Of A Sphere Enter: '6'\n For Sales Tax Enter: '7'\n For Canadian To AMerican Currency Exchange Enter: '8'")
+    input()
     if input=="1":
-        volume_of_square
-    if input=="2":
-    if input=="3":
-    if input=="4":
-    if input=="5":
-    if input=="6":
-    if input=="7":
-    if input=="8":
+     if input=="2":
+      if input=="3":
+       if input=="4":
+        if input=="5":
+         if input=="6":
+          if input=="7":
+            calcTax
+if input=="8":
+               
 
 
     # Will display a title screen
@@ -22,7 +24,7 @@ def menu():
     # Author: Copper
     # Modified:
     # title
-    return None
+ pass
 
 def instructions():
     # Will display instructions
@@ -40,10 +42,18 @@ def main():
     You will need to include a while loop to keep repeating the commands until
     the user chooses to exit
     """
-    title()
+
     while True:
         # keep giving options to choose menu options until they choose to exit
         pass
 
 if __name__ == "__main__":
     main()
+
+pass
+def calcTax():
+ x=input("Enter Product Price Before Tax")
+ tax=x*0.12
+ total=x+tax
+ print("Amount Of Taxes{tax}. Total Amount",total)
+pass
