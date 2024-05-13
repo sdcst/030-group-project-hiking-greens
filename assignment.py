d9ed121b26e31a7d62497c2909b9b8eb5d0c4551
fxrifty <logan.stevens@gmx.com>
2024-05-13 13:29:33 -0700

    save

--- assignment.py
+++ assignment.py
@@ -2,8 +2,7 @@
 # Volume Calculator
 # Feel free to rename your variables
 
-<<<<<<< HEAD
-=======
+
 import math
 def menu():
     print("For The Volume Of A Square Enter: '1'\n For The Area Of A Square Enter: '2'\n For The Volume Of A Rectangle Enter: '3'\n For The Area Of A Rectangle Enter: '4'\n For The Volume Of A Sphere Enter: '5'\n For The Area Of A Sphere Enter: '6'\n For Sales Tax Enter: '7'\n For Canadian To AMerican Currency Exchange Enter: '8'")
@@ -17,9 +16,7 @@ def menu():
           if input=="7":
             calcTax
 if input=="8":
-               
-
->>>>>>> 5989fd80564b266a0d8d165e35247ab83ff9f508
+ pass
 
 def title():
     # Will display a title screen
@@ -54,15 +51,14 @@ def main():
 if __name__ == "__main__":
     main()
 
-<<<<<<< HEAD
 
 
 def volume_of_sphere(r):
-    return  4/3 * pi * r^3
+    return  4/3 * math.pi * r^3
 
 
 def area_of_sphere(r):
-    return 4* pi * r^2
+    return 4* math.pi * r^2
 
 def area_of_spure(x):
     return x^2
@@ -78,10 +74,11 @@ def area_of_rectangle(W,L):
 
 
 
-def CAD_to_USD()
+def usd_to_cad(value):
+ value=input('Enter Amount In USD')
+ return value*1.37
     
  
-=======
 pass
 def calcTax():
  x=input("Enter Product Price Before Tax")
@@ -89,4 +86,4 @@ def calcTax():
  total=x+tax
  print("Amount Of Taxes{tax}. Total Amount",total)
 pass
->>>>>>> 5989fd80564b266a0d8d165e35247ab83ff9f508
+
